4d33b229dabf1876c35efabd46526dcdce1f6ee7
Salman <slmnkhokhar@gmail.com>
2014-06-05 16:15:49 -0700

    removed unnnecessary files

---
+++ findFilesWithTracks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  5 14:26:32 2014
+
+@author: skhokhar
+"""
+
+import os
+
+import os
+
+scenePath = "/home/skhokhar/common/people/skhokhar/KITTI/unzipped/campusUnzipped"
+path = scenePath+"/tracklets/"
+savePath = scenePath+"/synced_videos/"
